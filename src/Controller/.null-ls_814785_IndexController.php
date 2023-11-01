94f2882c3ed4748d00f8313eee2610cf7aba16c1
salarmotevalli <salar.mo.ro@gmail.com>
2023-11-01 14:13:12 +0330

    add index controller

---
+++ src/Controller/.null-ls_814785_IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/index', name: 'app_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/IndexController.php',
+        ]);
+    }
+}
